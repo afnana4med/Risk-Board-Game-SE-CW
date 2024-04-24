651db3c97d73fa7e6bf42d24e31b266936b31bcd
Afnan Ahmed <131989759+afnana4med@users.noreply.github.com>
2024-04-24 12:27:49 +0100

    commit

--- .godot/mono/temp/obj/Debug/Practice.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81951990511219efffc21479e951064ca9170c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
